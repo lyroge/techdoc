de4fd2f5c2b8658b8059ae3ad161e28f0e0320c0
songzhengang <songzhengang@breadtrip.com>
2016-05-25 14:23:19 +0800

    修改快速排序算法内容

--- algorithm/bubble_sort.py
+++ algorithm/bubble_sort.py
@@ -1,40 +1,24 @@
 # -*- coding: UTF-8 -*-
 """
-快速排序原理
+冒泡排序原理
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-选择一个值，然后将大于和小于他的值划分为两部分，然后递归对每个划分的集合再进行操作
-直到每一个集合都只有一个元素位置
+将元素两两比较，最大（小）的放到一端
 """
 
 import random
 
 
-def quick_sort(ary, start=0, end=0):
-    # 结束递归条件为元素个数小于1
-    if end - start <= 1:
-        return
-
-    # 选择第一个值为对比元素
-    index = start
-    flag_value = ary[start]
-    len_ls = end - start
-
-    # 将小于他的值放入他的左边
-    for i in range(start+1, end):
-        if ary[i] < flag_value:
-            min_v = ary.pop(i)
-            ary.insert(0, min_v)
-
-    # 找到标志值位置，然后划分两部分
-    index = ary.index(flag_value)
-    print index
-    quick_sort(ary, 0, index + 1)
-    quick_sort(ary, index + 1, len_ls)
+def bubble_sort(arg):
+    arg_len = len(arg)
+    for i in range(arg_len - 1):
+        for j in range(i + 1, arg_len):
+            if arg[i] > arg[j]:
+                arg[i], arg[j] = arg[j], arg[i]
 
 
 if __name__ == "__main__":
     test_ary = range(50)
     random.shuffle(test_ary)
     print test_ary
-    quick_sort(test_ary, 0, len(test_ary))
+    bubble_sort(test_ary)
     print test_ary
--- algorithm/quick_sort.py
+++ algorithm/quick_sort.py
@@ -1,24 +1,39 @@
 # -*- coding: UTF-8 -*-
 """
-冒泡排序原理
+快速排序原理
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-将元素两两比较，最大（小）的放到一端
+选择一个值，然后将大于和小于他的值划分为两部分，然后递归对每个划分的集合再进行操作
+直到每一个集合都只有一个元素位置
 """
 
 import random
 
 
-def bubble_sort(arg):
-    arg_len = len(arg)
-    for i in range(arg_len - 1):
-        for j in range(i + 1, arg_len):
-            if arg[i] > arg[j]:
-                arg[i], arg[j] = arg[j], arg[i]
+def first_sort(array, low, high):
+    key = array[low]
+    while low < high:
+        # 先处理最高上限的值降低到最小
+        while low < high and array[high] >= key:
+            high -= 1
+
+        while low < high and array[high] < key:
+            array[low] = array[high]
+            low += 1
+            array[high] = array[low]
+    array[low] = key
+    return low
 
 
-if __name__ == "__main__":
-    test_ary = range(50)
-    random.shuffle(test_ary)
-    print test_ary
-    bubble_sort(test_ary)
-    print test_ary
+def quick_sort(array, low, high):
+    if low < high:
+        key_index = first_sort(array, low, high)
+        quick_sort(array, low, key_index)
+        quick_sort(array, key_index + 1, high)
+
+
+if __name__ == '__main__':
+    array = range(100)
+    random.shuffle(array)
+    print array
+    quick_sort(array, 0, len(array) - 1)
+    print array
